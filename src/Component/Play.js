e6286cb60662b3869bd78bc151ddbf1a8ee0eef1
joeran <736165698@qq.com>
2017-11-27 00:19:51 +0800

    实现获取歌曲的时长，歌曲加载时间，进度条的样式，实现方法暂定“

--- src/Component/Play.js
+++ src/Component/Play.js
@@ -1,43 +1,68 @@
 import React, { Component } from 'react';
 import Icon from 'antd/lib/icon';
 import Avatar from 'antd/lib/avatar';
-import Progress from 'antd/lib/progress';
 import musicplay from './fly.mp3';
 import './Play.css';
 
-const src='./fly.mp3';
-const audioId=document.getElementById('audio');
+//const src='./fly.mp3';
 
 class Play extends Component {
   constructor(props){
     super(props);
     this.state = {
-      isPlay: false
+      isPlay: false,
+      currentTime: 0,
+      duration:0
     }
     this.handlePlayerClick = this.handlePlayerClick.bind(this);
+    this.timeStart = this.timeStart.bind(this);
+    this.controlAudio= this.controlAudio.bind(this);
   }
 
 componentDidMount(){
+}
 
+timeStart(time) {
+  let durationTime=parseInt(time);
+  let minute = parseInt(durationTime/60);
+  let second = durationTime%60+'';
+  let symbol = ':';
+  if(minute == 0){
+      minute = '00';
+  }else if(minute < 10 ){
+      minute = '0'+minute;
+  }
+  if(second.length == 1){
+    second = '0'+second;
+  }
+  return minute+symbol+second
+}
+
+controlAudio(){
+  this.setState({
+    duration:this.audioplay.duration,
+    currentTime:this.audioplay.currentTime
+  });
 }
 
 handlePlayerClick() {
   this.setState(prevState=>({
-    isPlay:!prevState.isPlay
+    isPlay:!prevState.isPlay,
+    //duration:this.audioplay.duration
   }));
   if(!this.state.isPlay) {
     this.audioplay.play();
     }else {
       this.audioplay.pause();
+      console.log(this.audioplay.currentTime);
+      console.log((parseInt(this.audioplay.currentTime))%100);
     }
 
 }
   render(){
-    console.log(this.audioplay);
-    console.log(this.isPlay);
     return(
       <div className="play">
-        <audio id="audio" preload="load" src={musicplay}  ref={(audio) => {this.audioplay=audio;}}>
+        <audio id="audio" preload="auto" src={musicplay} onTimeUpdate={ this.controlAudio}  ref={(audio) => {this.audioplay=audio;}}>
           您的浏览器不支持 audio 与元素。
         </audio>
         <Avatar shape="square" size="large" src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
@@ -48,7 +73,11 @@ handlePlayerClick() {
             </span>
            <span><Icon type="fast-forward" style={{ fontSize: 24, color: '#fff' }}/></span>
         </div>
-        <Progress className="play-progress" percent={0} />
+        <span style={{ fontSize: 12, color: '#fff' }}>{this.timeStart(this.state.currentTime)}</span>
+        <div className="play-progress">
+          <div className="progress-nav" style={{ width:10  }}></div>
+        </div>
+        <span style={{ fontSize: 12, color: '#fff' }}>{this.timeStart(this.state.duration)}</span>
       </div>
     );
   }
