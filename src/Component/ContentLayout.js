2831b14742161fd07625ec96558f00d6ed262ce2
joeran <736165698@qq.com>
2017-11-30 03:20:26 +0800

    fetch

--- src/Component/ContentLayout.js
+++ src/Component/ContentLayout.js
@@ -5,27 +5,46 @@ import './ContentLayout.css';
 
 
 class ContentLayout extends Component {
+
+  constructor() {
+    super();
+    this.state ={
+      songs:''
+    };
+  }
+
+  componentDidMount() {
+    var myFetchOptions ={
+      method:'GET'
+    };
+    fetch("https://route.showapi.com/1143-2?page=1&showapi_appid=51102&showapi_test_draft=false&showapi_timestamp=20171130015937&song_type=real-time&showapi_sign=4bf9544ec49e018dffba7f0eb654fcf3")
+    .then(response=>response.json())
+    .then(json=>this.setState({songs:json}));
+  };
   render(){
     const cols = [];
-      this.props.products.forEach((product) => {
-        cols.push(
-          <Col key={product.id} span={4}>
-            <div className="card">
-              <div className="card-image">
-                <img alt="example" width="100%" src={product.img} />
-              </div>
-              <div className="card-body" >
-                <h3>{product.dir}</h3>
-                <p>{product.name}</p>
-              </div>
-            </div>
-          </Col>
-        );
-      });
+    const songs=this.state;
+    const songsList = songs.length
+    ?
+    songs.map((songsItem,index)=>
+      <Col key={index} span={4}>
+        <div className="card">
+          <div className="card-image">
+            <img alt="example" width="100%" src={songsItem.showapi_res_body.albumLogo} />
+          </div>
+          <div className="card-body" >
+            <h3>{songsItem.showapi_res_body.songName}</h3>
+            <p>{songsItem.showapi_res_body.songName}</p>
+          </div>
+        </div>
+      </Col>
+  )
+  :
+  '没有加载到任何音乐';
     return(
       <div >
         <h2>新碟首发</h2>
-        <Row className="card-row" gutter={24}>{cols}</Row>
+        <Row className="card-row" gutter={24}>{songsList}</Row>
       </div>
     );
   }
--- src/Component/MainLayout.js
+++ src/Component/MainLayout.js
@@ -4,56 +4,9 @@ import Navbar from './Navbar';
 import MenuLayout from './MenuLayout';
 import ContentLayout from './ContentLayout';
 import Play from './Play';
-import music from './music.jpg';
-import music1 from './music1.jpg';
 import './MainLayout.css';
 
 const { Header, Sider, Content } = Layout;
-const assets =[
-  {
-    name:'张国荣',dir:'风继续吹，不忍远离',num:'2',img: music,id:1001
-  },
-  {
-    name:'孙燕姿',dir:'122435465',num:'6',img: music1,id:1002
-  },
-  {
-    name:'姚峰',dir:'122435465',num:'6',img: music,id:1003
-  },
-  {
-    name:'西瓜jun',dir:'啦啊啦啦',num:'6',img: music1,id:1004
-  },
-  {
-    name:'张国荣',dir:'风继续吹，不忍远离',num:'2',img: music,id:1005
-  },
-  {
-    name:'孙燕姿',dir:'122435465',num:'6',img: music1,id:1006
-  },
-  {
-    name:'姚峰',dir:'122435465',num:'6',img: music,id:1007
-  },
-  {
-    name:'西瓜jun',dir:'啦啊啦啦',num:'6',img: music1,id:1008
-  },
-  {
-    name:'张国荣',dir:'风继续吹，不忍远离',num:'2',img: music,id:1009
-  },
-  {
-    name:'孙燕姿',dir:'122435465',num:'6',img: music1,id:1010
-  },
-  {
-    name:'姚峰',dir:'122435465',num:'6',img: music,id:1011
-  },
-  {
-    name:'西瓜jun',dir:'啦啊啦啦',num:'6',img: music1,id:1012
-  },
-  {
-    name:'姚峰',dir:'122435465',num:'6',img: music,id:1013
-  },
-  {
-    name:'西瓜jun',dir:'啦啊啦啦',num:'6',img: music1,id:1014
-  },
-  
-]
 
 export default class MainLayout extends Component {
   render() {
@@ -72,7 +25,7 @@ export default class MainLayout extends Component {
               margin: 0,
               backgroundColor: 'white'
             }}>
-             <ContentLayout products={assets}/>
+             <ContentLayout/>
             </Content>
           </Layout>
           <Play />
